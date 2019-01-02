8b68c0cb92778ddbd9c69911ce08b2aa6b04e677
ongszehua <ongsh-pa16@student.tarc.edu.my>
2019-01-02 23:35:18 +0800

    new interface v2

---
+++ app/src/main/java/com/example/user/cioscarr/Choose_car_activity.java
@@ -0,0 +1,23 @@
+package com.example.user.cioscarr;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.user.cioscarr.main_navDrawer;
+
+public class Choose_car_activity extends main_navDrawer {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Car Details");
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_choose_car, null, false);
+        drawer.addView(contentView, 0);
+
+
+
+    }
+}
